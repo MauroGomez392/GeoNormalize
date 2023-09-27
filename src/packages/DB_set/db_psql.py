7d9682be2ac90d7b682abc69124e995aa98549d7
Mauro <73126563+MauroGomez392@users.noreply.github.com>
2023-09-27 09:11:31 -0300

    change library
    Change from dotenv to decouple

--- src/packages/DB_set/db_psql.py
+++ src/packages/DB_set/db_psql.py
@@ -1,17 +1,16 @@
 import os
-from dotenv import load_dotenv
+from decouple import config
 
-load_dotenv()
 #-----------------/Class with the ENV credentials/--------------------------
 class Psql:
     def __init__(self):
-        self.passW = os.getenv('DB_SECRET_KEY')
-        self.db_name = os.getenv('DB_NAME')
-        self.db_user = os.getenv('DB_USER')
-        self.db_host = os.getenv('DB_HOST')
-        self.db_port = os.getenv('DB_PORT')
-        self.logs_folder = os.getenv('LOGS_LOCAL_FOLDER')
-        self.project_location_path = os.getenv('PTOJECT_LOCATION_PATH')
+        self.passW = config('DB_SECRET_KEY')
+        self.db_name = config('DB_NAME')
+        self.db_user = config('DB_USER')
+        self.db_host = config('DB_HOST')
+        self.db_port = config('DB_PORT')
+        self.logs_folder = config('LOGS_LOCAL_FOLDER')
+        self.project_location_path = config('PTOJECT_LOCATION_PATH')
 #-----------------/Builds the URL for the connect/--------------------------
     def get_url_connect(self):
         #"postgresql://postgres:" + self.passW + "@localhost:5432/PruebaAsentamiento"
